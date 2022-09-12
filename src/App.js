f30705ba0528d9f80e930c919d73bfda8bdf4d53
Ching-wen Hu <ching-wen.hu@developersinstitute.co.nz>
2022-09-12 12:58:46 +1200

    add Routes, Route and relevant file components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import {Routes, Route} from "react-router-dom"
+import About from "./components/About"
+import Catering from "./components/Catering"
+import Location from "./components/Location"
+import Menu from "./components/Menu"
+import Reservations from "./components/Reservations"
+import Order from "./components/Order"
+import Layout from "./Layout/Layout"
+import Home from "./components/Home"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/about" element={<Layout />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/catering" element={<Catering />} />
+        <Route path="/location" element={<Location />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/reservations" element={<Reservations />} />
+        <Route path="/order" element={<Order />} />
+        <Route />
+      </Routes>
     </div>
   );
 }
