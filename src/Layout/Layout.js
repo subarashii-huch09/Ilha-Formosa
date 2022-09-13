2e7d223ba171715c81c8434417051760358c33a4
Ching-wen Hu <ching-wen.hu@developersinstitute.co.nz>
2022-09-13 19:07:45 +1200

    add Side component

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom"
 import Nav from"./Nav"
+import Footer from "./Footer"
+import Side from "./Side"
 
 const Layout = () =>{
   return (
     <>
       <Nav />
       <Outlet />
+      <Footer />
+      <Side />
     </>
   );
 }
